d16fc6a3ffe3ff2559f757d078af868dce32c59b
anurpeljto <anur.peljto@stu.ibu.edu.ba>
2025-12-03 13:53:11 +0100

    Move icons from devdep

---
+++ .next/static/chunks/repos_animations_app_favicon_ico_mjs_ce9a08bf._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/606f0_next_dist_50a1b23a._.js"
+],
+    source: "dynamic"
+});
